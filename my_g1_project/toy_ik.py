719d671fb6790b6e9802a0c83b65738f4e4fbcdd
hritik1204 <hritikraj1204@gmail.com>
2026-09-02 18:40:45 +0200

    Moved to a different folder SCENARIO_1_OBJ_PICK&PLACE_PARTITION

--- my_g1_project/toy_ik.py
+++ my_g1_project/toy_ik.py
@@ -1,24 +1,30 @@
-from ik_solver import solve_ik_jax, get_qpos_indices, LEFT_ARM_JOINT_NAMES, LEFT_PALM_SITE
 import numpy as np
 import mujoco
 from pathlib import Path
 from mujoco import mjx
+import jax
+import jax.numpy as jnp
+from time import time
+from mujoco import viewer
+
 
 SCENE_PATH = "scenarios/partition_task/scene.xml"
 
 model = mujoco.MjModel.from_xml_path(SCENE_PATH)
 data = mujoco.MjData(model)
 
+model.geom_contype[:] = 0
+model.geom_conaffinity[:] = 0
+
 mujoco.mj_forward(model, data)
 
+with mujoco.viewer.launch_passive(model, data) as viewer:
+
+    while viewer.is_running():
+        mujoco.mj_step(model, data)
+        viewer.sync()
+        time.sleep(0.0000005)
 
 
 
-x = np.array((1, 2, 3))
-y = np.array((4, 5, 6))
-
-z = x-y
-
-
-print(z)
 
